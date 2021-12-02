fbbfd427203739bd05865650077b5a16452b443b
Dhanush <suvarnadhanush2000@gmail.com>
2021-12-03 01:03:02 +0530

    update

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+//display side-bar in mobile view
 function show_menu() {
-    console.log("display side bar");
     const side_bar = document.getElementById('side-bar')
     const page = document.getElementById('page')
     side_bar.classList.add('show')
     page.classList.add('hide')
 }
 
+//hide side-bar in mobile view
 function hide_menu() {
-    console.log("hide side bar");
     const side_bar = document.getElementById('side-bar')
     const page = document.getElementById('page')
     side_bar.classList.remove('show')
     page.classList.remove('hide')
 }
+
