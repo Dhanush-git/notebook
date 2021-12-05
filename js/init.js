8dd40820583cd8dabc6cb521ac48cfdaf3462228
Dhanush <suvarnadhanush2000@gmail.com>
2021-12-05 20:40:53 +0530

    update

--- js/init.js
+++ js/init.js
@@ -1,13 +1,70 @@
 var mydb = JSON.parse(localStorage.getItem('db'))
+const navBar = document.getElementsByClassName('nav-bar')[0]
+
 
 if (mydb == null) {
 
-    const data = [{"page-id":1,"page-name":"Personal","page-data":[{"task-id":1,"task-name":"create new task","status":"false"}]}]
+    const data = [{"page-id":1,"page-name":"Personal","page-data":[{"task-id":1,"task-name":"create new task","status":false}]}]
 
     localStorage.setItem("db",JSON.stringify(data))
+    localStorage.setItem("current_index",0)
+    location.reload()
 }else{
 
     if (mydb.length != 0) {
-        console.log("no data in db");
+        populateNav()
+        populatePage()
     }
 }
+
+
+function populateNav() {
+    mydb.forEach(e => {
+        const new_button = document.createElement("div")
+        new_button.className = "button"
+        new_button.id = e['page-id']
+    
+        const new_title = document.createElement("div")
+        new_title.className = "button-page-title"
+        new_title.innerHTML = e['page-name']
+    
+        const new_page_notification = document.createElement('div')
+        new_page_notification.className = "button-page-notification"
+        new_page_notification.innerHTML = "0"
+    
+        new_button.appendChild(new_title)
+        new_button.appendChild(new_page_notification)
+    
+        navBar.appendChild(new_button)
+    });
+}
+
+function populatePage() {
+    const id = localStorage.getItem("current_index")
+    const page = document.getElementsByClassName('page-content-wrapper')[0]
+    const mydata = JSON.parse(localStorage.getItem('db'))
+
+    page.innerHTML = " "
+
+    const page_name = document.createElement("div")
+    page_name.className = "page-name"
+    page_name.id = mydata[id]['page-id']
+    page_name.innerHTML = mydata[id]['page-name']
+
+    const todo_list = document.createElement("div")
+    todo_list.classList = "todo-list"
+    
+    mydata[id]['page-data'].forEach(e=>{
+        const new_li = document.createElement('li')
+        const new_input = document.createElement('input')
+        new_input.type = "checkbox"
+
+        new_li.appendChild(new_input)
+        new_li.append(" "+e['task-name'])
+        todo_list.appendChild(new_li)
+    })
+
+    page.appendChild(page_name)
+    page.appendChild(todo_list)
+}
+
--- js/rename.js
+++ js/rename.js
@@ -1,16 +1,59 @@
 const pageName = document.getElementsByClassName('page-name')[0]
-pageName.addEventListener("dblclick",rename)
+pageName.addEventListener("dblclick",function(e){rename(e.target.id)})
 
-function rename() {
+const addPageButton = document.getElementById('add-page-button')
+addPageButton.addEventListener("click",addPage)
+
+const pageNav = document.getElementsByClassName('nav-bar')[0]
+const pages = Array.from(pageNav.children)
+
+pages.forEach(e=>{
+    e.addEventListener("click",function(){
+        const i = pages.indexOf(e)
+        localStorage.setItem("current_index",i)
+        location.reload()
+    })
+})
+
+function rename(id) {
+    var mydata = JSON.parse(localStorage.getItem('db'))
+    
     const input = document.createElement('input')
     input.value = pageName.innerHTML
 
     pageName.innerHTML=""
     pageName.append(input)
     input.focus()
-
+    
     input.addEventListener("focusout",function(e){
         pageName.removeChild(input)
         pageName.innerHTML = e.target.value
+
+        for (let i=0; i < mydata.length; i++) {
+            if (mydata[i]['page-id']==id) {
+                mydata[i]['page-name']=e.target.value
+                localStorage.setItem('db',JSON.stringify(mydata))
+                location.reload()
+            }
+        }
     })
+
+
 }
+
+
+function addPage() {
+    const mydata = JSON.parse(localStorage.getItem('db'))
+    var new_page = '';
+    if (mydata == null) {
+        new_page = {'page-id':1,'page-name':'New Page','page-data':[{'task-id':1,'task-name':'new task','status':false}]}
+        
+    }else{
+        new_page = {'page-id':mydata.at(-1)['page-id']+1,'page-name':'New Page','page-data':[{'task-id':1,'task-name':'new task','status':false}]}
+    }
+
+    mydata.push(new_page)
+    localStorage.setItem('db',JSON.stringify(mydata))
+    location.reload()
+}
+
